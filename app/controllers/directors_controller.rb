fde0f9137f7e89df4b8b8c87740eae5fc0fcf4d3
Titus Chu <tituschu@uchicago.edu>
2023-04-21 05:46:42 +0000

    first commit

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,9 @@
+class DirectorsController < ApplicationController
+
+  def index
+    @list_of_directors = Director.all
+    
+  render ({ :template => "director_templates/index.html.erb" })
+  end
+
+end
