42b7f2a5b9a9b7250c9200b18dcbf72f129ef0b9
Titus Chu <tituschu@uchicago.edu>
2023-04-21 06:59:29 +0000

    2

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,4 +6,20 @@ class DirectorsController < ApplicationController
   render ({ :template => "director_templates/index.html.erb" })
   end
 
+
+  def wisest
+    @oldest = Director.where.not({ :dob => nil}).order({ :dob => :asc }).at(0)
+    
+  render ({ :template => "director_templates/eldest.html.erb" })
+  end
+
+  def director_details
+    # params looks like this {"an_id"=>"42"}
+
+     the_id = params.fetch("an_id")
+
+     @the_director = Director.where({ :id => the_id}).at(0)
+
+    render ({ :template => "director_templates/show.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors/eldest", { :controller => "directors", :action => "wisest" })
 
   get("/directors", { :controller => "directors", :action => "index" })
+
+
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+
+  get("/", { :controller => "application", :action => "homepage" })
+
+
 end
